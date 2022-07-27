cd5936b73ac98189f6c0a11d55a29da346eae6ec
Bruno Castro Brunckhorst <brunobrunckhorst@gmail.com>
2022-07-27 09:54:41 +0200

    Created EmployeesRepo

---
+++ app/repositories/employee_repository.rb
@@ -0,0 +1,34 @@
+require 'csv'
+require_relative '../models/employee'
+
+class EmployeeRepository
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @employees = []
+
+    load_csv if File.exist?(csv_file)
+  end
+
+  def all
+    @employees
+  end
+
+  def find(id)
+    @employees.find { |employee| employee.id == id }
+  end
+
+  def find_by_username(username)
+    @employees.find { |employee| employee.username == username }
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |row|
+      # row => {id: 1, username: 'bruncky', password: 'secret', role: 'manager'}
+      row[:id] = row[:id].to_i
+
+      @employees << Employee.new(row)
+    end
+  end
+end

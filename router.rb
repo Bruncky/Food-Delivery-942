48a3414b7b553ce865263cb8160d4f40380d6563
Bruno Castro Brunckhorst <brunobrunckhorst@gmail.com>
2022-07-27 09:53:52 +0200

    Adapted the router with different employee menus

--- router.rb
+++ router.rb
@@ -1,22 +1,39 @@
 class Router
-  def initialize(meals_controller, customers_controller)
+  def initialize(meals_controller, customers_controller, sessions_controller)
     @meals_controller = meals_controller
     @customers_controller = customers_controller
+    @sessions_controller = sessions_controller
+
     @running = true
   end
 
   def run
+    employee = @sessions_controller.sign_in
+
     while @running
-      print_menu
-      choice = gets.chomp.to_i
-      print `clear`
-      route_action(choice)
+      # Check employee's role
+      # Print manager menu OR driver menu
+      if employee.manager?
+        print_manager_menu
+
+        choice = gets.chomp.to_i
+        print `clear`
+
+        route_manager_action(choice)
+      else
+        print_driver_menu
+
+        choice = gets.chomp.to_i
+        print `clear`
+
+        route_driver_action(choice)
+      end
     end
   end
 
   private
 
-  def print_menu
+  def print_manager_menu
     puts "--------------------"
     puts "------- MENU -------"
     puts "--------------------"
@@ -28,12 +45,31 @@ class Router
     print "> "
   end
 
-  def route_action(choice)
+  def print_driver_menu
+    puts "--------------------"
+    puts "------- MENU -------"
+    puts "--------------------"
+    puts "1. List all meals"
+    puts "2. List all customers"
+    puts "8. Exit"
+    print "> "
+  end
+
+  def route_manager_action(choice)
     case choice
     when 1 then @meals_controller.add
     when 2 then @meals_controller.list
     when 3 then @customers_controller.add
     when 4 then @customers_controller.list
+    when 8 then stop!
+    else puts "Try again..."
+    end
+  end
+
+  def route_driver_action(choice)
+    case choice
+    when 1 then @meals_controller.list
+    when 2 then @customers_controller.list
     when 8 then stop!
     else puts "Try again..."
     end

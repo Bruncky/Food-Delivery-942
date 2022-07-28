cd7790262781e9ecaf01737db13ff194d01aecd2
Bruno Castro Brunckhorst <brunobrunckhorst@gmail.com>
2022-07-28 11:28:20 +0200

    Adapted menus for manager and rider

--- router.rb
+++ router.rb
@@ -35,24 +35,26 @@ class Router
   private
 
   def print_manager_menu
-    puts "--------------------"
-    puts "------- MENU -------"
-    puts "--------------------"
-    puts "1. Add new meal"
-    puts "2. List all meals"
-    puts "3. Add new customer"
-    puts "4. List all customers"
-    puts "8. Exit"
-    print "> "
+    puts '--------------------'
+    puts '------- MENU -------'
+    puts '--------------------'
+    puts '1. Add new meal'
+    puts '2. List all meals'
+    puts '3. Add new customer'
+    puts '4. List all customers'
+    puts '5. Add new order'
+    puts '6. List all undelivered orders'
+    puts '7. Exit'
+    print '> '
   end
 
   def print_rider_menu
     puts "--------------------"
     puts "------- MENU -------"
     puts "--------------------"
-    puts "1. List all meals"
-    puts "2. List all customers"
-    puts "8. Exit"
+    puts "1. List my undelivered orders"
+    puts "2. Mark order as delivered"
+    puts "3. Exit"
     print "> "
   end
 
@@ -62,16 +64,18 @@ class Router
     when 2 then @meals_controller.list
     when 3 then @customers_controller.add
     when 4 then @customers_controller.list
-    when 8 then stop!
+    when 5 then @orders_controller.add
+    when 6 then @orders_controller.list_undelivered_orders
+    when 7 then stop!
     else puts "Try again..."
     end
   end
 
   def route_rider_action(choice)
     case choice
-    when 1 then @meals_controller.list
-    when 2 then @customers_controller.list
-    when 8 then stop!
+    when 1 then @orders_controller.list_my_undelivered_orders(@current_user)
+    when 2 then @orders_controller.mark_as_delivered(@current_user)
+    when 3 then stop!
     else puts "Try again..."
     end
   end

3a3251a4d1cc78d3f179f2fdb85279fec2b3824f
Bruno Castro Brunckhorst <brunobrunckhorst@gmail.com>
2022-07-28 12:42:46 +0200

    Created OrderRepository

---
+++ app/repositories/order_repository.rb
@@ -0,0 +1,67 @@
+class OrderRepository
+  def initialize(csv_file, meal_repository, customer_repository, employee_repository)
+    @csv_file = csv_file
+    
+    # Auxiliary repos
+    @meal_repository = meal_repository
+    @customer_repository = customer_repository
+    @employee_repository = employee_repository
+
+    @next_id = 1
+    @orders = []
+
+    load_csv if File.exist?(csv_file)
+  end
+
+  def create(order)
+    order.id = @next_id
+    @orders << order
+
+    @next_id += 1
+
+    save_csv
+  end
+
+  def undelivered_orders
+    @orders.reject { |order| order.delivered? }
+  end
+
+  def my_undelivered_orders(current_user)
+    @orders.reject { |order| order.delivered? && order.employee.username != current_user.username }
+  end
+
+  # def mark_as_delivered(order)
+  #   order.deliver!
+  # end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file, headers: :first_row, header_converters: :symbol) do |row|
+      # row => {id: 1, delivered: true, meal_id: 1, customer_id: 2, employee_id: 3}
+      row[:id] = row[:id].to_i
+      row[:delivered] = row[:delivered] == 'true'
+
+      # Finding instances
+      row[:meal] = @meal_repository.find(row[:meal_id].to_i)
+      row[:customer] = @customer_repository.find(row[:customer_id].to_i)
+      row[:employee] = @employee_repository.find(row[:employee_id].to_i)
+
+      #                       Order.new({id: 1, delivered: true,  meal_id: 1, customer_id: 2, employee_id: 3, meal: INSTANCE, customer: INSTANCE, employee: INSTANCE})
+      @orders << Order.new(row)
+    end
+
+    @next_id = @orders.last.id + 1 unless @orders.empty?
+  end
+
+  def save_csv
+    CSV.open(@csv_file, 'wb') do |csv|
+      # csv << ['id', 'delivered', 'meal_id', 'customer_id', 'employee_id']
+      csv << %w[id delivered meal_id customer_id employee_id]
+
+      @orders.each do |order|
+        csv << [order.id, order.delivered?, order.meal.id, order.customer.id, order.employee.id]
+      end
+    end
+  end
+end
